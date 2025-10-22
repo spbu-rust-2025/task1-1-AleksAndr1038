4cbff4d41f213da73fbe4e6dbe7a97a71e49b833
Александр Смоликов <aleksandrsmolikov107@gmail.com>
2025-10-22 20:59:34 +0300

    program for summing two numbers

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use std::io;
 
 fn main() {
     let mut input: String = String::new();
-	io::stdin().read_line(&mut input).unwrap();
+	io::stdin().read_line(&mut input).expect("Faild to read line");
 
-	let mut nums = input.split_whitespace().map(|x| x.parse::<i32>().unwrap());
-	let sum = nums.next().unwrap() + nums.next().unwrap();
-	
-    println!("{}", sum);
+	let mut scanner = input.split_whitespace();
+	let a: i32 = scanner.next().unwrap().parse().unwrap();
+	let b: i32 = scanner.next().unwrap().parse().unwrap();
+
+	println!("{}", a + b);
 }
