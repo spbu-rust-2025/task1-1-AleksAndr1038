5c97d2ee142d736678f0f7b98b8c9bfb9b237e0c
Александр Смоликов <aleksandrsmolikov107@gmail.com>
2025-10-22 21:08:59 +0300

    Programm for summing two numbers

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use std::io;
 
 fn main() {
     let mut input: String = String::new();
-	io::stdin().read_line(&mut input).expect("Faild to read line");
+	io::stdin().read_line(&mut input).unwrap();
 
 	let mut scanner = input.split_whitespace();
 	let a: i32 = scanner.next().unwrap().parse().unwrap();
 	let b: i32 = scanner.next().unwrap().parse().unwrap();
-
-	println!("{}", a + b);
+	
+    println!("{}", a + b);
 }
