5b5e2d8b150d6f9f7e2114b40c359fa1923119be
Александр Смоликов <aleksandrsmolikov107@gmail.com>
2025-10-22 15:41:22 +0300

    program for adding two numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut input: String = String::new();
+	io::stdin().read_line(&mut input).unwrap();
+
+	let mut nums = input.split_whitespace().map(|x| x.parse::<i32>().unwrap());
+	let sum = nums.next().unwrap() + nums.next().unwrap();
+	
+    println!("{}", sum);
 }
